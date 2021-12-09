f1bcd19e273e45c4686174cb3ab5224e7e965f25
mialeshka <mialeshka1985@gmail.com>
2021-12-09 09:16:20 +0300

    update

--- BookShop/src/main/java/it/mialeshka/repository/BookFileRepository.java
+++
@@ -1,9 +0,0 @@
-package it.mialeshka.repository;
-
-import it.mialeshka.entity.BookFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookFileRepository extends JpaRepository<BookFile, Long> {
-}
--- BookShop/target/BookShop/WEB-INF/classes/main.js
+++
@@ -1,8 +0,0 @@
-import Vue  from 'vue'
-import App from 'pages/App.vue'
-
-new Vue({
-    el: '#app',
-    render: a =>a(App)
-})
-
